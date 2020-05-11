163be2a341f3b3d23ab6fba07a723e6e501d5441
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-05-11 19:40:09 +0300

    Add our familiar form

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const router = new express.Router();
 
-/* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.render("index");
 });
 
 module.exports = router;
