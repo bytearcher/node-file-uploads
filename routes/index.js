dd2c5378695a6189532838d7f8ce76892d75b7c1
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-05-13 09:16:12 +0300

    Process file upload request with busboy

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+const Busboy = require("busboy");
 const express = require("express");
 
 const router = new express.Router();
 
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.post("/", (req, res, next) => {
+  const busboy = new Busboy({ headers: req.headers });
+
+  busboy.on("field", (name, value) => {
+    console.log(`${name}: ${value}`);
+  });
+  busboy.on("file", (name, stream, filename, encoding, contentType) => {
+    console.log(`${name}: ${filename} (${contentType})`);
+    stream.resume();
+  });
+  busboy.on("finish", () => {
+    res.send("OK");
+  });
+
+  req.pipe(busboy);
 });
 
 module.exports = router;
