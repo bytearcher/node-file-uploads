2c13d2eb3ef30045319367f6b70c792b58268b01
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-07-11 09:18:40 +0300

    Design database schema

---
+++ backend/schema/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE product (
+  id           SERIAL PRIMARY KEY,
+  title        VARCHAR(255)   NOT NULL,
+  manufacturer VARCHAR(255)   NOT NULL,
+  price        NUMERIC(11, 2) NOT NULL,
+  description  TEXT
+);
+
+CREATE TABLE image (
+  id           SERIAL PRIMARY KEY,
+  product_id   INTEGER REFERENCES product (id),
+  image        BYTEA        NOT NULL,
+  content_type VARCHAR(255) NOT NULL
+);
