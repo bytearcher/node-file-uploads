340317d0ab4f546696ef5201aee81dd2ecf06b01
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-08-18 14:53:01 +0300

    Validate multipart/form-data submit data

--- backend/src/service/createNewProduct.js
+++ backend/src/service/createNewProduct.js
@@ -1,14 +1,31 @@
+const Joi = require("@hapi/joi");
+
 const { connect } = require("../integration/productDatabase");
+
+const schema = Joi.object({
+  title: Joi.string().required().trim(),
+  manufacturer: Joi.string().required().trim(),
+  price: Joi.number().required(),
+  description: Joi.string().required().trim(),
+});
+
+function validateAndSanitizeProduct(rawFields) {
+  const validationResult = schema.validate(rawFields);
+  if (validationResult.error) {
+    throw validationResult;
+  }
+  return validationResult.value;
+}
 
 class NewProductCreator {
   constructor() {
-    this.product = {};
+    this.rawFields = {};
     this.imageIds = [];
     this.productDatabasePromise = connect();
   }
 
   async addField(name, value) {
-    this.product[name] = value;
+    this.rawFields[name] = value;
   }
 
   async addFile(name, stream, filename, contentType) {
@@ -21,7 +38,8 @@ class NewProductCreator {
 
   async finish() {
     const productDatabase = await this.productDatabasePromise;
-    const productId = await productDatabase.insertProduct(this.product);
+    const product = validateAndSanitizeProduct(this.rawFields);
+    const productId = await productDatabase.insertProduct(product);
     await productDatabase.updateProductIdForImages(productId, this.imageIds);
 
     await productDatabase.release();
