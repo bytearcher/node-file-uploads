5476131a2c34f60a35486b6e414b03d09bb00fd0
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-08-27 15:28:41 +0300

    Implement route handler for get product image

--- backend/src/api/routes/getProductImage.js
+++ backend/src/api/routes/getProductImage.js
@@ -1,16 +1,25 @@
 const asyncHandler = require("express-async-handler");
+const stream = require("stream");
+const util = require("util");
 
 const { getProductImage } = require("../../service/getProductImage");
 
+const pipeline = util.promisify(stream.pipeline);
+
 async function getProductImageHandler(req, res, next) {
-  const image = await getProductImage();
+  const { productId, imageId } = req.params;
+
+  const image = await getProductImage(productId, imageId);
+  if (!image) {
+    return res.sendStatus(404);
+  }
 
   res.set({
     "Content-Type": image.contentType,
     "Content-Length": image.contentLength,
   });
 
-  res.send(image.data);
+  await pipeline(image.stream, res);
 }
 
 const getProductImageRoute = [asyncHandler(getProductImageHandler)];
