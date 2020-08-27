30f284723a6dbf5d749e4bd88e0308d1e34ce5d6
Panu Pitkamaki <pspi@users.noreply.github.com>
2020-08-27 10:46:32 +0300

    Organize database code with classes

--- backend/src/integration/productDatabase.js
+++ backend/src/integration/productDatabase.js
@@ -1,11 +1,26 @@
-async function insertProduct(product) {
-  const { title, manufacturer, price, description } = product;
-  const response = await connection.query(
-    `
-              INSERT INTO product (title, manufacturer, price, description)
-              VALUES ($1, $2, $3, $4)
-              RETURNING id`,
-    [title, manufacturer, price, description]
-  );
-  return response.rows[0].id;
+async function connect() {
+  const connection;
+  return new ProductDatabase(connection);
+}
+
+class ProductDatabase {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  async insertProduct(product) {
+    const { title, manufacturer, price, description } = product;
+    const response = await this.connection.query(
+      `
+                INSERT INTO product (title, manufacturer, price, description)
+                VALUES ($1, $2, $3, $4)
+                RETURNING id`,
+      [title, manufacturer, price, description]
+    );
+    return response.rows[0].id;
+  }
+
+  async release() {
+    await this.connection.release();
+  }
 }
